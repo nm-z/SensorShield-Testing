f667d2b83281960595b7b8deaf2a84e82e8046b2
nm-z <138162007+nm-z@users.noreply.github.com>
2025-06-19 18:55:18 -0700

    Update serial stub for live data

--- tests/stubs/serial.py
+++ tests/stubs/serial.py
@@ -1,5 +1,18 @@
 class Serial:
+    """Simple stub of ``pyserial.Serial`` supporting dump and live modes."""
+
     def __init__(self, *_, **__):
+        self._index = 0
+        self._set_live_data()
+
+    def _set_live_data(self):
+        self._data = (
+            b"Booting...\n"
+            b"Data logged: {\"temp\":25,\"humidity\":60}\n"
+            b"Data logged: {\"temp\":26,\"humidity\":61}\n"
+        )
+
+    def _set_dump_data(self):
         self._data = b"DUMP_START\n{\"temp\":25,\"humidity\":60}\nDUMP_END\n"
         self._index = 0
 
@@ -7,20 +20,24 @@ class Serial:
     def in_waiting(self):
         return len(self._data) - self._index
 
-    def write(self, _):
-        pass
+    def write(self, data):
+        if b"DUMP_ALL" in data:
+            self._set_dump_data()
 
     def read(self, size=1):
         start = self._index
         end = min(start + size, len(self._data))
+        chunk = self._data[start:end]
         self._index = end
-        return self._data[start:end]
+        return chunk
 
     def readline(self):
         newline = self._data.find(b"\n", self._index)
         if newline == -1:
             newline = len(self._data)
-        end = newline + 1
+            end = newline
+        else:
+            end = newline + 1
         chunk = self._data[self._index:end]
         self._index = end
         return chunk
@@ -30,3 +47,4 @@ class Serial:
 
 class SerialException(Exception):
     pass
+
